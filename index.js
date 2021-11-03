2f01e5d125aa3c8f3f36757f75368dbdafe77916
Doris Morales <moralesdoris880@gmail.com>
2021-11-03 11:46:58 -0400

    commit tree 4578fdd8df2c65561e3660388cf87b82a243b0d7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    const winner = record.find (({result})=>result === "W");
+    if (winner === undefined){
+        return winner;
+    }
+    else{
+        return winner['year'];
+    }
+}
